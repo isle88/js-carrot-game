344d86381056be7845f18e3f2e71207ee750ab5d
isle88 <hyunjungkim2014@gmail.com>
2022-10-26 17:32:06 +0100

    refactored main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 "use strict";
 import PopUP from "./popup.js";
-const CARROT_SIZE = 80;
+import Field from "./field.js";
+import * as sound from "./sound.js";
+
 const CARROT_COUNT = 10;
 const BUG_COUNT = 10;
 const GAME_DURATION_SEC = 60;
@@ -8,27 +10,16 @@ const GAME_DURATION_SEC = 60;
 const gameBtn = document.querySelector(".game__button");
 const gameCounter = document.querySelector(".game__counter");
 const gameTimer = document.querySelector(".game__timer");
-const gameField = document.querySelector(".game__field");
-const fieldRect = gameField.getBoundingClientRect();
-
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
-const alertSound = new Audio("./sound/alert.wav");
-const bgSound = new Audio("./sound/bg.mp3");
-const winSound = new Audio("./sound/game_win.mp3");
-const failSound = new Audio("./sound/bug_pull.mp3");
 let started = false;
 let carrotCounter = 0;
 let timer = undefined;
 
-const gameFinishBanner = new PopUP();
-
 function initGame() {
-  gameField.innerHTML = "";
   gameCounter.innerHTML = CARROT_COUNT;
-  addItem("carrot", CARROT_COUNT, "img/carrot.png");
-  addItem("bug", BUG_COUNT, "img/bug.png");
+  gameField.init();
 }
 
+gameBtn.addEventListener("click", controlGame);
 function controlGame() {
   if (started) {
     stopGame();
@@ -36,6 +27,31 @@ function controlGame() {
     startGame();
   }
 }
+
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickLister(onItemClick);
+
+function onItemClick(item) {
+  if (!started) {
+    return;
+  }
+  if (item === "carrot") {
+    carrotCounter--;
+    gameCounter.innerText = carrotCounter;
+
+    if (carrotCounter === 0) {
+      stopGame();
+      finishGame(true);
+    }
+  } else if (item === "bug") {
+    finishGame(false);
+  }
+}
+
+const gameFinishBanner = new PopUP();
+gameFinishBanner.setClickListener(() => {
+  startGame();
+});
 
 function startGame() {
   started = true;
@@ -45,7 +61,7 @@ function startGame() {
   showTimeAndCounter();
   startGameTimer();
   showPlayBtn();
-  playSound(bgSound);
+  sound.playBg();
 }
 
 function stopGame() {
@@ -56,20 +72,20 @@ function stopGame() {
   stopGameTimer();
   hidePlayBtn();
   gameFinishBanner.showWithText("REPLAY?");
-  playSound(alertSound);
-  stopSound(bgSound);
+  sound.playAlert();
+  sound.stopBg();
 }
 
 function finishGame(win) {
   started = false;
   hidePlayBtn();
   if (win) {
-    playSound(winSound);
+    sound.playWin();
   } else {
-    playSound(failSound);
+    sound.playFail();
   }
   stopGameTimer();
-  stopSound(bgSound);
+  sound.stopBg();
   gameFinishBanner.showWithText(win ? "Yay :)" : "Try again?");
 }
 
@@ -108,63 +124,3 @@ function updateTimerText(time) {
 function hidePlayBtn() {
   gameBtn.style.visibility = "hidden";
 }
-
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width - CARROT_SIZE;
-  const y2 = fieldRect.height - CARROT_SIZE;
-
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement("img");
-    item.setAttribute("class", className);
-    item.setAttribute("src", imgPath);
-    item.style.position = "absolute";
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
-    gameField.appendChild(item);
-    carrotCounter = count;
-    gameCounter.innerHTML = carrotCounter;
-  }
-
-  function randomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-}
-
-function removeCarrot(event) {
-  if (!started) {
-    return;
-  }
-  const target = event.target;
-  if (target.matches(".carrot")) {
-    event.target.remove();
-    carrotCounter--;
-    playSound(carrotSound);
-    gameCounter.innerText = carrotCounter;
-
-    if (carrotCounter === 0) {
-      stopGame();
-      finishGame(true);
-    }
-  } else if (target.matches(".bug")) {
-    finishGame(false);
-  }
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
-}
-
-function stopSound(sound) {
-  sound.pause();
-}
-
-gameBtn.addEventListener("click", controlGame);
-gameField.addEventListener("click", removeCarrot);
-gameFinishBanner.setClickListener(() => {
-  startGame();
-});
